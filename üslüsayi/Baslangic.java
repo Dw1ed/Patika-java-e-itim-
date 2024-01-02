d78afa24d1ab748502bc5969fcce2c20aa215e91
Dw1ed <113717873+Dw1ed@users.noreply.github.com>
2024-01-02 16:17:01 +0300

    Add files via upload

---
+++ üslüsayi/Baslangic.java
@@ -0,0 +1,27 @@
+
+package üslüsayi;
+import java.util.Scanner;
+
+
+public class Baslangic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int base, power, number;
+        System.out.print("Please enter base: ");
+        base = scanner.nextInt();
+        number = base;
+        System.out.print("Please enter power: ");
+        power = scanner.nextInt();
+        if (power == 0) {
+            System.out.println(1);
+        } else if (power == 1) {
+            System.out.println(number);
+        } else {
+            for (int i = 1; i < power; i++) {
+                number = number * number;
+            }
+            System.out.println(number);
+        }
+
+    }
+}
