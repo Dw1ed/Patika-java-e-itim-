dd43627139687b9c3f65b024e55da2207079e183
Dw1ed <113717873+Dw1ed@users.noreply.github.com>
2024-01-02 10:16:05 +0300

    Add files via upload
    girilen sayıya kadar olan cift sayıları sıralama

--- Giris/Baslangic.java
+++ Giris/Baslangic.java
@@ -6,47 +6,19 @@ import java.util.Scanner;
 
 public class Baslangic {
     public static void main(String[] args) {
-        int dg;
-        Scanner girdi=new Scanner(System.in);
-        System.out.println("Doğrum yılınızı giriniz");
-        dg=girdi.nextInt();
-        switch (dg){
-            case 0:
-                    System.out.println("Çin Zodyağınız Maymundur");
-                    break;
-            case 1:
-                System.out.println("Çin Zodyağınız Horozdur");
-                break;
-            case 2:
-                System.out.println("Çin Zodyağınız Köpektir");
-                break;
-            case 3:
-                System.out.println("Çin Zodyağınız Köpektir");
-                break;
-            case 4:
-                System.out.println("Çin Zodyağınız Faredir");
-                break;
-            case 5:
-                System.out.println("Çin Zodyağınız Öküzdür");
-                break;
-            case 6:
-                System.out.println("Çin Zodyağınız Kaplandır");
-                break;
-            case 7:
-                System.out.println("Çin Zodyağınız Tavşandır");
-                break;
-            case 8:
-                System.out.println("Çin Zodyağınız Ejderhadır");
-                break;
-            case 9:
-                System.out.println("Çin Zodyağınız Yılandır");
-                break;
-            case 10:
-                System.out.println("Çin Zodyağınız Atdır");
-                break;
-            case 11:
-                System.out.println("Çin Zodyağınız Koyundur");
-                break;
-        }
+       int k;
+       Scanner inp=new Scanner(System.in);
+       System.out.println("Sayı giriniz: ");
+       k=inp.nextInt();
+       int i=0;
+       while(i<=k){
+           if(i%2==0){
+               System.out.println(i);
+           }
+           i++;
+       }
+
+
+
     }
 }
