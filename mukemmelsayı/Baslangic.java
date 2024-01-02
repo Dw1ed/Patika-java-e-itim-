20b2bb5c030044789907bc99d174126625059c64
Dw1ed <113717873+Dw1ed@users.noreply.github.com>
2024-01-02 16:41:15 +0300

    Add files via upload

---
+++ mukemmelsayı/Baslangic.java
@@ -0,0 +1,33 @@
+
+package mukemmelsayı;
+import java.util.Scanner;
+
+public class Baslangic {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("----------------------");
+        System.out.println("Mükemmel Sayı Bulma");
+        System.out.println("----------------------");
+        System.out.print("Bir sayı giriniz: ");
+
+        int sayi = input.nextInt();
+        int toplam = 0;
+
+        for (int i = 1; i < sayi; i++)
+        {
+            if (sayi % i == 0)
+            {
+                toplam += i;
+            }
+        }
+        if (toplam == sayi)
+        {
+            System.out.println(sayi + " \nBir mükemmel sayıdır!");
+        }
+        else
+        {
+            System.out.println(sayi + " \nBir mükemmel sayı değildir!");
+        }
+
+    }
+}
