9c8ee9f7b25d5e01362044d88bcbef284e1d99f1
Dw1ed <113717873+Dw1ed@users.noreply.github.com>
2024-01-06 00:08:07 +0300

    Add files via upload

---
+++ mayinTarlasi/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+            public static void main(String[] args) {
+
+                MainSweeper mainSweeper = new MainSweeper(3,3);
+                mainSweeper.run();
+
+            }
+        }
+
